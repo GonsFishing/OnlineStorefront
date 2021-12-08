42b64b7a18b282fd22c1935ca246d8cfeaa36b74
Sixten Gons Larsson <sixtenglarsson@gmail.com>
2021-12-08 15:14:42 +0100

    Added shopping cart

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -51,6 +51,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
+  test "can't delete product in cart" do
+    assert_difference('product.count', 0) do
+      delete product_url(Products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
