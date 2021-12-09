8c01080757d543cbca7d2e8de39315c3ded49191
Sixten Gons Larsson <sixtenglarsson@gmail.com>
2021-12-09 11:35:49 +0100

    Fixed bugs and added test with line_items

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -52,8 +52,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "can't delete product in cart" do
-    assert_difference('product.count', 0) do
-      delete product_url(Products(:two))
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
     end
 
     assert_redirected_to products_url
