da12a57bf2418ae710da19cdb06c0c23aa24d97e
Sixten Gons Larsson <sixtenglarsson@gmail.com>
2021-12-10 11:11:59 +0100

    Updated line_items tests

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -23,7 +23,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     follow_redirect!
 
     assert_select 'h2', 'Your shopping cart'
-    assert_select 'li', 'Yuuto'
+    assert_select 'li.product', "1 \u00D7 Yuuto"
   end
 
   test "should show line_item" do
@@ -37,7 +37,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item), params: { line_item: { product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
