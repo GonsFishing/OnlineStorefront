4dcd7c0d2de1373753f6b3874efe0d464deead6e
Sixten Gons Larsson <sixtenglarsson@gmail.com>
2021-12-13 09:34:54 +0100

    Styling refactoring and bugfixes

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,14 +1,16 @@
 class LineItemsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:create]
-  before_action :set_line_item, only: %i[ show edit update destroy ]
+  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
-  # GET /line_items or /line_items.json
+  # GET /line_items 
+  #GET /line_items.json
   def index
     @line_items = LineItem.all
   end
 
-  # GET /line_items/1 or /line_items/1.json
+  # GET /line_items/1 
+  # GET /line_items/1.json
   def show
   end
 
@@ -28,23 +30,27 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to store_index_url }
+        format.js
+        format.json { render :show, 
+          status: :created, location: @line_item }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render :new }
+        format.json { render json: @line_item.errors, 
+          status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /line_items/1 or /line_items/1.json
+  # PATCH/PUT /line_items/1 
+  #PATCH/PUT /line_items/1.json
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
         format.html { redirect_to @line_item, notice: "Line item was successfully updated." }
         format.json { render :show, status: :ok, location: @line_item }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
@@ -67,6 +73,6 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
